86fcc68a74f0bbc0df0853d8c70aa6664e708ed6
Benoît Mendousse <benoit-mendousse@mailoo.org>
2017-02-24 22:24:42 +0100

    Add name of the host the dev server is listening on.

--- run_devserver.js
+++ run_devserver.js
@@ -5,6 +5,6 @@ const host = process.argv[3] || "localhost"
 
 const srv = new DevServer()
 srv.on("listening", () => {
-	console.log(`Listening on port ${port}.`)
+	console.log(`Listening on port ${port}, host ${host}.`)
 })
 srv.listen({port, host})
