334a7ef57146b9a5cee24a9f5025a01a10e7ba60
Benoît Mendousse <benoit-mendousse@mailoo.org>
2017-02-24 22:22:21 +0100

    Removes warnings from rollup about external dependencies.

--- rollup.config.js
+++ rollup.config.js
@@ -2,5 +2,6 @@ import async_to_gen from "rollup-plugin-async"
 
 export default {
 	plugins: [ async_to_gen() ],
-	moduleName: "agendadulibre"
+	moduleName: "agendadulibre",
+	external: [ "http", "fs", "url", "stream", "tape" ]
 }
