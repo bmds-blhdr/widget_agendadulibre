911914ebf21da75dd0aac95eb7846f3c48d630fe
Benoît Mendousse <benoit-mendousse@mailoo.org>
2017-02-27 01:03:41 +0100

    Plugin to do basic cleanup of distributed source.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import async_to_gen from "rollup-plugin-async"
+import cleanup from "rollup-plugin-cleanup"
 
 export default {
-	plugins: [ async_to_gen() ],
+	plugins: [ async_to_gen(), cleanup() ],
 	moduleName: "agendadulibre",
 	external: [ "http", "fs", "url", "stream", "tape" ]
 }
